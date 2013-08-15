92ca476b9a3b6a76f1dae971acf26ed11a2b25cd
David Batelu <davidbatelu@gmail.com>
2013-08-15 09:52:21 -0400

    PCA file name fix. Sda over 2000 features

--- code/PCA.py
+++ code/PCA.py
@@ -44,19 +44,21 @@ pca = RandomizedPCA(n_components=2000)
 # lil_train = train.tolil()
 # train_new = lil2(lil_train)
 
+#pca.fit(train)
 
-train = pca.fit_transform(train)
-test = pca.transform(test)
-valid = pca.transform(valid)
 
-set1 = (test, test_labels)
-set2 = (valid, valid_labels)
-set3 = (train, train_labels)
+train_red = pca.fit_transform(train)
+valid_red = pca.transform(valid)
+test_red = pca.transform(test)
+
+set1 = (test_red, test_labels)
+set2 = (valid_red, valid_labels)
+set3 = (train_red, train_labels)
 
 try:
     cPickle.dump(set1, open(data_path + "test" + ".PCA.sparse.pkl", "w"))
     cPickle.dump(set2, open(data_path + "validation" + ".PCA.sparse.pkl", "w"))
     cPickle.dump(set3, open(data_path + "train" + ".PCA.sparse.pkl", "w"))
-    cPickle.dump((set3, set2, set1), gzip.open(data_path + "PCA.sparse.pkl", "wb"))
+    cPickle.dump((set3, set2, set1), gzip.open(data_path + "PCA.sparse.pkl.gz", "wb"))
 except:
     import pdb; pdb.set_trace()
