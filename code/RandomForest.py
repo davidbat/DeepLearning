c6c8f916f6e1b3a37fb4bde8fdf02f9de4b97d95
David Batelu <davidbatelu@gmail.com>
2013-10-24 15:24:29 -0400

    Adding stash stuff. Might break things. Recheck this commit if things go awry

--- code/RandomForest.py
+++ code/RandomForest.py
@@ -2,9 +2,19 @@ from sklearn.cross_validation import cross_val_score
 from sklearn.ensemble import RandomForestClassifier
 import cPickle
 
-X, y = cPickle.load(open("../data/train.sparse.pkl"))
+X, y = cPickle.load(open("../data/RF.train.sparse.pkl"))
 X = X.toarray()
+Xtest, ytest = cPickle.load(open("../data/RF.test.sparse.pkl"))
+Xt = Xtest.toarray()
 
-clf = RandomForestClassifier(n_estimators=1000, min_samples_split=3)
-scores = cross_val_score(clf, X, y)
-print scores.mean()
+clf = RandomForestClassifier(n_estimators=500, min_samples_split=3)
+#print "Doing a cross validation"
+#scores = cross_val_score(clf, X, y)
+#print scores.mean()
+
+print "Fitting"
+clf.fit(X, y)
+
+
+print "Testing"
+print clf.score(Xt, ytest)
