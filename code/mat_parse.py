7cc3c78e0b6dffd37da09a3e4e85fccb1f16a43f
David Batelu <davidbatelu@gmail.com>
2013-11-14 18:38:16 -0500

    Adding log messages

--- code/mat_parse.py
+++ code/mat_parse.py
@@ -2,6 +2,11 @@ from scipy.io import loadmat
 import cPickle
 import gzip
 import numpy as np
+import logging
+FORMAT = '%(asctime)-15s: %(message)s'
+logging.basicConfig(format=FORMAT, filename='../results/mat_parse.log', level=logging.DEBUG)
+logger = logging.getLogger()
+
 
 def get_min_max(arr):
 	trp = np.transpose(arr)
@@ -35,6 +40,7 @@ def jumper(lst, jump=1):
 
 def main():
 	ip_fn = "../data/20Newsgroups.mat"
+	logger.info("Load form the matlab dataset.")
 	mat = loadmat(ip_fn)
 	test_idx = mat['testIdx']
 	train_idx = mat['trainIdx']
@@ -42,27 +48,32 @@ def main():
 	labels = mat['gnd']
 	data_path = "../data/"
 
+	logger.info("Getting min max list")
 	min_max = get_min_max(data)
+	logger.info("Applying Z normalization")
 	data = apply_min_max(data, min_max)
 
 
 	jump = 10
 	valid_perc = 10
+	logger.info("Getting the indices for each dataset.")
 	valid_idx = map(lambda i: mat['trainIdx'][i][0],
 					filter(lambda j: j % valid_perc == 0, range(len(mat['trainIdx']))))
 	train_idx = map(lambda i: mat['trainIdx'][i][0],
 					filter(lambda j: j % valid_perc != 0, range(len(mat['trainIdx']))))
 	test_idx = [ item[0] for item in mat['testIdx'].tolist() ]
 
+	logger.info("Reducing the datasets.")
 	train_idx = jumper(train_idx, jump)
 	valid_idx = jumper(valid_idx, jump)
 	test_idx = jumper(test_idx, jump)
 
-
+	logger.info("Getting the labels.")
 	train_labels = map(lambda i: mat['gnd'][i[0]-1][0], train_idx)
 	valid_labels = map(lambda i: mat['gnd'][i[0]-1][0], valid_idx)
 	test_labels = map(lambda i: mat['gnd'][i[0]-1][0], test_idx)
 
+	logger.info("Getting the data.")
 	train = map(lambda i: data[i[0]-1], train_idx)
 	valid = map(lambda i: data[i[0]-1], valid_idx)
 	test = map(lambda i: data[i[0]-1], test_idx)
@@ -71,6 +82,7 @@ def main():
 	set2 = (valid, valid_labels)
 	set3 = (test, test_labels)
 
+	logger.info("Pickling the data.")
 	cPickle.dump(set1, open(data_path + "train" + "_mat.pkl", "w"))
 	cPickle.dump(set2, open(data_path + "valid" + "_mat.pkl", "w"))
 	cPickle.dump(set3, open(data_path + "test" + "_mat.pkl", "w"))
